1ee16d3df4c67efd2f360cd47843d4f649da3bde
Ian McClaskey <imcclaskey@gmail.com>
2025-05-13 23:22:51 -0400

    Bump version to 0.0.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current d3 version
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 // VersionWithPrefix returns the version with 'v' prefix
 func VersionWithPrefix() string {
